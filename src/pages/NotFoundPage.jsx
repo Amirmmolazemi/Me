d90d1c6abf2a6e58bc9c5202e7c039ede3a1e0ec
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-01 23:47:47 +0330

    feat:add a 404 page if you write a wrong router

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+import { useTitle } from "../hooks/useTitle.js";
+
+import styles from "./styles/NotFoundPage.module.css";
+import picture from "../assets/404-page-not-found.svg";
 
 function NotFoundPage() {
+  useTitle("404");
   return (
-    <div>NotFoundPage</div>
-  )
+    <div className={styles.container}>
+      <img src={picture} alt="notFound" />
+      <h1>404</h1>
+      <p>Oops! Page Not Found</p>
+      <Link to="/Me">Go Back To Homepage</Link>
+    </div>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
