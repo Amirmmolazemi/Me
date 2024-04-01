6287ab5cc01ce97321a4123405ada635e0015abd
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-01 23:47:20 +0330

    fix:fix the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Layout from "./Layout/Layout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
 
 function App() {
   return (
     <>
       <Layout>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/Me" element={<HomePage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/Me" element={<HomePage />} />
+          <Route path="/Me/contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </Layout>
     </>
   );
