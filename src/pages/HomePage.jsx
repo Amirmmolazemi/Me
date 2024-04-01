c6bc9e0ae5b6e33322fc0edb1fee0923f941abe2
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-01 23:48:34 +0330

    feat:add a home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,33 @@
+import { useTitle } from "../hooks/useTitle.js";
+
+import { Link } from "react-router-dom";
+
+import styles from "./styles/HomePage.module.css";
+
+import picture from "../assets/picture.jpg";
+
 function HomePage() {
+  useTitle("Home")
   return (
-    <>
-      <h1>HomePage</h1>
-    </>
+    <div className={styles.home}>
+      <div className={styles.homeContent}>
+        <h1>hi , i'm amirmohammad</h1>
+        <h3>FrontEnd Developer</h3>
+        <p>
+          Hi everyone ! I'm AmirMohammad and i tend to develop myself at
+          front-end developing to become professional 😊
+          <br />
+          check my CV on About Tab And also you can Contact with me 📞
+        </p>
+        <div className={styles.btnBox}>
+          <Link to="/Me">My CV</Link>
+          <Link to="/Me/contact">Let's talk</Link>
+        </div>
+      </div>
+      <div className={styles.homeImage}>
+        <img src={picture} alt="picture" />
+      </div>
+    </div>
   );
 }
 
