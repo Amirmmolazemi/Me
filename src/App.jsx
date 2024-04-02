4a7ad4f62009b55d7c134b70d9d5e5ad64d1ecaf
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-02 15:49:25 +0330

    fix:add some new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { Route, Routes } from "react-router-dom";
 
 import Layout from "./Layout/Layout.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import NotFoundPage from "./pages/NotFoundPage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
+import PortfolioPage from "./pages/PortfolioPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/Me" element={<HomePage />} />
           <Route path="/Me/contact" element={<ContactPage />} />
+          <Route path="/Me/portfolio" element={<PortfolioPage />} />
+          <Route path="/Me/about-me" element={<AboutPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Layout>
