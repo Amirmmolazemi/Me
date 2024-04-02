3f1c7e651c5e83fa3d07ba46b2289bde8a7bfb9b
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-02 15:49:10 +0330

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from "./App.jsx";
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/">
+    <App />
+  </BrowserRouter>
 );
