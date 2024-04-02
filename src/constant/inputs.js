b765f95f56c99c86854f988015dd0d367afd8b37
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-02 13:12:58 +0330

    perf:try to write clean code

--- src/constant/inputs.js
+++ src/constant/inputs.js
@@ -1,11 +1,12 @@
-const firstRowInputs = [
-  { id: "name", name: "name", type: "text", placeholder: "Full Name" },
-  { id: "email", name: "email", type: "text", placeholder: "Email Address" },
+const inputs = [
+  [
+    { id: "name", name: "name", type: "text", placeholder: "Full Name" },
+    { id: "email", name: "email", type: "text", placeholder: "Email Address" },
+  ],
+  [
+    { id: "phone", name: "phone", type: "tel", placeholder: "Phone Number" },
+    { id: "subject", name: "sunject", type: "text", placeholder: "Subject" },
+  ],
 ];
 
-const secondRowInputs = [
-  { id: "phone", name: "phone", type: "tel", placeholder: "Phone Number" },
-  { id: "subject", name: "sunject", type: "text", placeholder: "Subject" },
-];
-
-export { firstRowInputs, secondRowInputs };
+export { inputs };
