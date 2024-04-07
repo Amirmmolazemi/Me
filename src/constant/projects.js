1d51c03ba9ab14eab8e963d35bbe5ba4f8880524
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-07 21:29:31 +0330

    a js file that shows my projects

---
+++ src/constant/projects.js
@@ -0,0 +1,34 @@
+const projects = [
+  {
+    id: "React_Contact_App",
+    name: "React_Contact_App",
+    url: "https://github.com/Amirmmolazemi/React-Contact-App",
+  },
+  {
+    id: "React-book-App",
+    name: "React-book-App",
+    url: "https://github.com/Amirmmolazemi/React-book-App",
+  },
+  {
+    id: "React-Crypto-App",
+    name: "React-Crypto-App",
+    url: "https://github.com/Amirmmolazemi/React-Crypto-App",
+  },
+  {
+    id: "React-Context-Store",
+    name: "React-Context-Store",
+    url: "https://github.com/Amirmmolazemi/React-Context-Store-Project",
+  },
+  {
+    id: "Redux-Toolkit-Store",
+    name: "Redux-Toolkit-Store",
+    url: "https://github.com/Amirmmolazemi/Redux-Toolkit-Store-Project",
+  },
+  {
+    id: "React-Blog-App",
+    name: "React-Blog-App",
+    url: "https://github.com/Amirmmolazemi/React-Blog-App",
+  },
+];
+
+export default projects;
