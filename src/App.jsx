d3bdf28fa97172ada4098a1e5339fd9cbb922fef
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-07 21:27:35 +0330

    remove about me page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Layout from "./Layout/Layout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import PortfolioPage from "./pages/PortfolioPage.jsx";
-import AboutPage from "./pages/AboutPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
           <Route path="/Me" element={<HomePage />} />
           <Route path="/Me/contact" element={<ContactPage />} />
           <Route path="/Me/portfolio" element={<PortfolioPage />} />
-          <Route path="/Me/about-me" element={<AboutPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Layout>
