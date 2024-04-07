9df271d98d0d32a552885ebca9fb2e37c469d762
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-07 23:07:45 +0330

    feat:add a page that shows the project

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -3,15 +3,31 @@ import { useTitle } from "../hooks/useTitle";
 import projects from "../constant/projects";
 import PortfolioCard from "../components/PortfolioCard";
 
+import { Grid, Typography } from "@mui/material";
+
 function PortfolioPage() {
   useTitle("portfolio");
   return (
-    <div>
-      <h1>Projects that you can see in my github</h1>
+    <Grid container spacing={2}>
+      <Grid item xs={12} mb={5}>
+        <Typography variant="h3" component="h3" textAlign="center">
+          you can see my projects in this page
+        </Typography>
+      </Grid>
       {projects.map((project) => (
-        <PortfolioCard project={project} key={project.id} />
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          key={project.id}
+          display="flex"
+          justifyContent="center"
+        >
+          <PortfolioCard project={project} />
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 }
 
