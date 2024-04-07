1ce006658057e47e9b00dc5de7280cd133074256
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-08 00:04:08 +0330

    feat:add a sender email

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,33 +1,27 @@
 import Swal from "sweetalert2";
+import emailjs from "@emailjs/browser";
 
 const sendEmail = (fullName, email, number, body, subject) => {
-  alert("enter");
-  //   const bodyMessage = `Full Name: ${fullName}
-  //       <br/>
-  //       Email: ${email}
-  //       <br/>
-  //       Phone Number: ${number}
-  //       <br/>
-  //       message: ${body}
-  //       `;
+  const templateParams = `Full Name: ${fullName}
+        <br/>
+        Email: ${email}
+        <br/>
+        Phone Number: ${number}
+        <br/>
+        message: ${body}
+        `;
 
-  //   Email.send({
-  //     Host: "smtp.elasticemail.com",
-  //     Username: "am.molazemi@gmail.com",
-  //     Password: "86E7AC6268111901695DBEA869020F12A7F8",
-  //     To: "am.molazemi@gmail.com",
-  //     From: email,
-  //     Subject: subject,
-  //     Body: bodyMessage,
-  //   }).then((message) => {
-  //     if (message == "OK") {
-  //       Swal.fire({
-  //         title: "Success",
-  //         text: "Message Sent Successfully",
-  //         icon: "success",
-  //       });
-  //     }
-  //   });
+  emailjs
+    .send("service_t9wmwtn", "template_f7wwyqi", templateParams)
+    .then((message) => {
+      if (message == "OK") {
+        Swal.fire({
+          title: "Success",
+          text: "Message Sent Successfully",
+          icon: "success",
+        });
+      }
+    });
 };
 
 export { sendEmail };
