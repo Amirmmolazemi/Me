126a1866d87f289c0d2c59471d20585f43e4ee20
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-07 21:30:35 +0330

    feat:add a contact me page that you can send me email

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,13 +1,64 @@
+import { useState } from "react";
 import { useTitle } from "../hooks/useTitle.js";
 
+import { sendEmail } from "../helpers/helper.js";
 import { inputs } from "../constant/inputs.js";
-
-import InputBox from "../components/InputBox.jsx";
+import Swal from "sweetalert2";
 
 import styles from "./styles/ContactPage.module.css";
 
 function ContactPage() {
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    body: "",
+  });
   useTitle("contact");
+
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    const valu = event.target.value;
+
+    setValue((items) => ({
+      ...items,
+      [name]: valu,
+    }));
+  };
+
+  // useEffect(() => {
+  //   console.log(value);
+  // }, [value.name, value.email, value.body, value.subject, value.phone]);
+
+  const clickHandler = () => {
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const phoneRegex =
+      /(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/;
+
+    if (
+      !value.name.length > 0 &&
+      !emailRegex.test(value.email) &&
+      !phoneRegex.test(value.phone) &&
+      !value.subject.length > 0 &&
+      !value.body.length > 0
+    ) {
+      Swal.fire({
+        title: "Invalid Data",
+        text: "Your Message Is Not Valid Please Fix Your Message",
+        icon: "error",
+      });
+    } else {
+      sendEmail(
+        value.name,
+        value.email,
+        value.phone,
+        value.body,
+        value.subject
+      );
+    }
+  };
 
   return (
     <div className={styles.contact}>
@@ -15,26 +66,61 @@ function ContactPage() {
       <form action="#" className={styles.contactForm}>
         <div className={styles.inputBox}>
           {inputs[0].map((input) => (
-            <InputBox data={input} key={input.id} styles={styles} />
+            <div
+              className={`${styles.inputField} ${styles.field}`}
+              key={input.id}
+            >
+              <input
+                type={input.type}
+                placeholder={input.placeholder}
+                name={input.name}
+                autoComplete="off"
+                className={styles.item}
+                value={value[input.name]}
+                onChange={changeHandler}
+              />
+              <div className={styles.errorText}>
+                invalid {input.placeholder}
+              </div>
+            </div>
           ))}
         </div>
+
         <div className={styles.inputBox}>
           {inputs[1].map((input) => (
-            <InputBox data={input} key={input.id} styles={styles} />
+            <div
+              className={`${styles.inputField} ${styles.field}`}
+              key={input.id}
+            >
+              <input
+                type={input.type}
+                placeholder={input.placeholder}
+                name={input.name}
+                autoComplete="off"
+                className={styles.item}
+                value={value[input.name]}
+                onChange={changeHandler}
+              />
+              <div className={styles.errorText}>
+                invalid {input.placeholder}
+              </div>
+            </div>
           ))}
         </div>
+
         <div className={`${styles.textAriaField} ${styles.field}`}>
           <textarea
-            cols="30"
             rows="10"
             placeholder="Your Message"
             className={styles.item}
             autoComplete="off"
+            name="body"
+            onChange={changeHandler}
           ></textarea>
           <div className={styles.errorText}>message can not be empty</div>
         </div>
 
-        <button type="submit">Send Message</button>
+        <button onClick={clickHandler}>Send Message</button>
       </form>
     </div>
   );
