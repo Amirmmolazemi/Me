a918a4d54648024aee5c082a736189dc4c119607
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-10 02:36:50 -0700

    refactor the array

--- src/constant/inputs.js
+++ src/constant/inputs.js
@@ -1,12 +1,8 @@
 const inputs = [
-  [
-    { id: "name", name: "name", type: "text", placeholder: "Full Name" },
-    { id: "email", name: "email", type: "text", placeholder: "Email Address" },
-  ],
-  [
-    { id: "phone", name: "phone", type: "tel", placeholder: "Phone Number" },
-    { id: "subject", name: "subject", type: "text", placeholder: "Subject" },
-  ],
+  { id: "name", name: "name", type: "text", placeholder: "Full Name" },
+  { id: "email", name: "email", type: "text", placeholder: "Email Address" },
+  { id: "phone", name: "phone", type: "tel", placeholder: "Phone Number" },
+  { id: "subject", name: "subject", type: "text", placeholder: "Subject" },
 ];
 
 export { inputs };
