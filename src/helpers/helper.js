fc0235386065ff1b14f01a52a31d6bf8d68a23eb
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-10 01:27:04 -0700

    feat:update helper.js to send email

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -3,25 +3,36 @@ import emailjs from "@emailjs/browser";
 
 const sendEmail = (fullName, email, number, body, subject) => {
   const templateParams = `Full Name: ${fullName}
-        <br/>
-        Email: ${email}
-        <br/>
-        Phone Number: ${number}
-        <br/>
-        message: ${body}
-        `;
+    <br/> 
+    subject: ${subject}
+    <br/>
+    Email: ${email}
+    <br/>
+    Phone Number: ${number}
+    <br/>
+    message: ${body}
+   `;
 
   emailjs
-    .send("service_t9wmwtn", "template_f7wwyqi", templateParams)
-    .then((message) => {
-      if (message == "OK") {
+    .sendForm("service_mh7wcdq", "template_jmeqzr8", templateParams, {
+      publicKey: "8c8E1YzE9bfpmIGQx",
+    })
+    .then(
+      () => {
         Swal.fire({
           title: "Success",
           text: "Message Sent Successfully",
           icon: "success",
         });
+      },
+      () => {
+        Swal.fire({
+          title: "failed",
+          text: "Message Sent Unsuccessfully",
+          icon: "error",
+        });
       }
-    });
+    );
 };
 
 export { sendEmail };
