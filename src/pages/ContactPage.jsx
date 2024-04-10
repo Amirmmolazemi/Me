2fe356ecee7d6ec90d4bdd95b175f38736725848
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-10 02:36:25 -0700

    fix:fix the email bug

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useTitle } from "../hooks/useTitle.js";
 
 import { sendEmail } from "../helpers/helper.js";
@@ -8,6 +8,7 @@ import Swal from "sweetalert2";
 import styles from "./styles/ContactPage.module.css";
 
 function ContactPage() {
+  const form = useRef();
   const [value, setValue] = useState({
     name: "",
     email: "",
@@ -15,7 +16,6 @@ function ContactPage() {
     subject: "",
     body: "",
   });
-  useTitle("contact");
 
   const changeHandler = (event) => {
     const name = event.target.name;
@@ -27,96 +27,54 @@ function ContactPage() {
     }));
   };
 
+  useTitle("contact");
+
   const clickHandler = (event) => {
     event.preventDefault();
+    console.log(form);
     const emailRegex =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const phoneRegex =
       /(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/;
 
     if (
-      !value.name.length > 0 &&
-      !emailRegex.test(value.email) &&
-      !phoneRegex.test(value.phone) &&
-      !value.subject.length > 0 &&
-      !value.body.length > 0
+      value.name.length > 0 &&
+      emailRegex.test(value.email) &&
+      phoneRegex.test(value.phone) &&
+      value.subject.length > 0 &&
+      value.body.length > 0
     ) {
+      sendEmail(form);
+    } else {
       Swal.fire({
         title: "Invalid Data",
         text: "Your Message Is Not Valid Please Fix Your Message",
         icon: "error",
       });
-    } else {
-      sendEmail(
-        value.name,
-        value.email,
-        value.phone,
-        value.body,
-        value.subject
-      );
     }
   };
 
   return (
     <div className={styles.contact}>
       <h2>Contact Me !</h2>
-      <form className={styles.contactForm}>
-        <div className={styles.inputBox}>
-          {inputs[0].map((input) => (
-            <div
-              className={`${styles.inputField} ${styles.field}`}
-              key={input.id}
-            >
-              <input
-                type={input.type}
-                placeholder={input.placeholder}
-                name={input.name}
-                autoComplete="off"
-                className={styles.item}
-                value={value[input.name]}
-                onChange={changeHandler}
-              />
-              <div className={styles.errorText}>
-                invalid {input.placeholder}
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className={styles.inputBox}>
-          {inputs[1].map((input) => (
-            <div
-              className={`${styles.inputField} ${styles.field}`}
-              key={input.id}
-            >
-              <input
-                type={input.type}
-                placeholder={input.placeholder}
-                name={input.name}
-                autoComplete="off"
-                className={styles.item}
-                value={value[input.name]}
-                onChange={changeHandler}
-              />
-              <div className={styles.errorText}>
-                invalid {input.placeholder}
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className={`${styles.textAriaField} ${styles.field}`}>
-          <textarea
-            rows="10"
-            placeholder="Your Message"
-            className={styles.item}
+      <form className={styles.contactForm} ref={form}>
+        {inputs.map((input) => (
+          <input
+            type={input.type}
+            key={input.id}
+            name={input.name}
             autoComplete="off"
-            name="body"
+            placeholder={input.placeholder}
+            value={value[input.name]}
             onChange={changeHandler}
-          ></textarea>
-          <div className={styles.errorText}>message can not be empty</div>
-        </div>
-
+          />
+        ))}
+        <textarea
+          rows="10"
+          placeholder="Your Message"
+          autoComplete="off"
+          name="body"
+        ></textarea>
         <button onClick={clickHandler}>Send Message</button>
       </form>
     </div>
