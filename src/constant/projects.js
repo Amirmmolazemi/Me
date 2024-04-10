ae17084295ff119d329cea263f08dae36da75817
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-10 00:48:23 -0700

    feat:add images to the portfolio

--- src/constant/projects.js
+++ src/constant/projects.js
@@ -1,33 +1,45 @@
+import BlogApp from "../assets/pictures/BlogApp.png";
+import BookApp from "../assets/pictures/BookApp.png";
+import ContactApp from "../assets/pictures/ContactApp.png";
+import CryptoApp from "../assets/pictures/CryptoApp.png";
+import StoreProject from "../assets/pictures/StoreProject.png";
+
 const projects = [
   {
     id: "React_Contact_App",
     name: "React_Contact_App",
     url: "https://github.com/Amirmmolazemi/React-Contact-App",
+    src: ContactApp,
   },
   {
     id: "React-book-App",
     name: "React-book-App",
     url: "https://github.com/Amirmmolazemi/React-book-App",
+    src: BookApp,
   },
   {
     id: "React-Crypto-App",
     name: "React-Crypto-App",
     url: "https://github.com/Amirmmolazemi/React-Crypto-App",
+    src: CryptoApp,
   },
   {
     id: "React-Context-Store",
     name: "React-Context-Store",
     url: "https://github.com/Amirmmolazemi/React-Context-Store-Project",
+    src: StoreProject,
   },
   {
     id: "Redux-Toolkit-Store",
     name: "Redux-Toolkit-Store",
     url: "https://github.com/Amirmmolazemi/Redux-Toolkit-Store-Project",
+    src: StoreProject,
   },
   {
     id: "React-Blog-App",
     name: "React-Blog-App",
     url: "https://github.com/Amirmmolazemi/React-Blog-App",
+    src: BlogApp,
   },
 ];
 
