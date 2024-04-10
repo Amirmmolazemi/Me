8d4f5f7b19c1242a5fa92fb6a258dc126bb5f4ba
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-10 02:36:37 -0700

    fix:fix the email bug

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,20 +1,9 @@
 import Swal from "sweetalert2";
 import emailjs from "@emailjs/browser";
 
-const sendEmail = (fullName, email, number, body, subject) => {
-  const templateParams = `Full Name: ${fullName}
-    <br/> 
-    subject: ${subject}
-    <br/>
-    Email: ${email}
-    <br/>
-    Phone Number: ${number}
-    <br/>
-    message: ${body}
-   `;
-
+const sendEmail = (form) => {
   emailjs
-    .sendForm("service_mh7wcdq", "template_jmeqzr8", templateParams, {
+    .sendForm("service_mh7wcdq", "template_jmeqzr8", form.current, {
       publicKey: "8c8E1YzE9bfpmIGQx",
     })
     .then(
@@ -25,12 +14,13 @@ const sendEmail = (fullName, email, number, body, subject) => {
           icon: "success",
         });
       },
-      () => {
+      (error) => {
         Swal.fire({
           title: "failed",
           text: "Message Sent Unsuccessfully",
           icon: "error",
         });
+        console.log(error);
       }
     );
 };
