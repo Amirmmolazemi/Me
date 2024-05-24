6948ad4da15b34f1e80ffb71401c0f088aee8f67
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-05-24 10:44:51 +0330

    add a assignment page

---
+++ src/pages/Assignment.jsx
@@ -0,0 +1,44 @@
+import styles from "./styles/Assignment.module.css";
+
+function Assignment() {
+  return (
+    <div className={styles.container}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sit
+      voluptates quis alias molestias accusamus veniam, at aspernatur temporibus
+      dicta, est veritatis cupiditate dolorum ullam voluptas quaerat vitae
+      excepturi corrupti. Architecto aliquam eveniet vero reprehenderit,
+      explicabo quaerat minima cum id! Optio distinctio architecto molestias
+      quos illo necessitatibus quisquam, nesciunt incidunt reiciendis ipsa,
+      harum quia illum eum. Labore deserunt dolor debitis, doloremque recusandae
+      neque eveniet ex in dignissimos, eum nesciunt consequatur, aspernatur
+      nihil corporis? Earum accusamus tempore officia nobis! Magnam ratione
+      soluta error, rem dolorem expedita excepturi cumque beatae culpa fugiat
+      officiis, nihil asperiores? Distinctio similique ea quia dolorum non
+      culpa!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sit
+      voluptates quis alias molestias accusamus veniam, at aspernatur temporibus
+      dicta, est veritatis cupiditate dolorum ullam voluptas quaerat vitae
+      excepturi corrupti. Architecto aliquam eveniet vero reprehenderit,
+      explicabo quaerat minima cum id! Optio distinctio architecto molestias
+      quos illo necessitatibus quisquam, nesciunt incidunt reiciendis ipsa,
+      harum quia illum eum. Labore deserunt dolor debitis, doloremque recusandae
+      neque eveniet ex in dignissimos, eum nesciunt consequatur, aspernatur
+      nihil corporis? Earum accusamus tempore officia nobis! Magnam ratione
+      soluta error, rem dolorem expedita excepturi cumque beatae culpa fugiat
+      officiis, nihil asperiores? Distinctio similique ea quia dolorum non
+      culpa!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sit
+      voluptates quis alias molestias accusamus veniam, at aspernatur temporibus
+      dicta, est veritatis cupiditate dolorum ullam voluptas quaerat vitae
+      excepturi corrupti. Architecto aliquam eveniet vero reprehenderit,
+      explicabo quaerat minima cum id! Optio distinctio architecto molestias
+      quos illo necessitatibus quisquam, nesciunt incidunt reiciendis ipsa,
+      harum quia illum eum. Labore deserunt dolor debitis, doloremque recusandae
+      neque eveniet ex in dignissimos, eum nesciunt consequatur, aspernatur
+      nihil corporis? Earum accusamus tempore officia nobis! Magnam ratione
+      soluta error, rem dolorem expedita excepturi cumque beatae culpa fugiat
+      officiis, nihil asperiores? Distinctio similique ea quia dolorum non
+      culpa!
+    </div>
+  );
+}
+
+export default Assignment;
