5405708979d8c0fd5ee78813f211849e62da279b
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-05-26 12:06:50 +0330

    add the writing

--- src/pages/Assignment.jsx
+++ src/pages/Assignment.jsx
@@ -3,40 +3,35 @@ import styles from "./styles/Assignment.module.css";
 function Assignment() {
   return (
     <div className={styles.container}>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sit
-      voluptates quis alias molestias accusamus veniam, at aspernatur temporibus
-      dicta, est veritatis cupiditate dolorum ullam voluptas quaerat vitae
-      excepturi corrupti. Architecto aliquam eveniet vero reprehenderit,
-      explicabo quaerat minima cum id! Optio distinctio architecto molestias
-      quos illo necessitatibus quisquam, nesciunt incidunt reiciendis ipsa,
-      harum quia illum eum. Labore deserunt dolor debitis, doloremque recusandae
-      neque eveniet ex in dignissimos, eum nesciunt consequatur, aspernatur
-      nihil corporis? Earum accusamus tempore officia nobis! Magnam ratione
-      soluta error, rem dolorem expedita excepturi cumque beatae culpa fugiat
-      officiis, nihil asperiores? Distinctio similique ea quia dolorum non
-      culpa!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sit
-      voluptates quis alias molestias accusamus veniam, at aspernatur temporibus
-      dicta, est veritatis cupiditate dolorum ullam voluptas quaerat vitae
-      excepturi corrupti. Architecto aliquam eveniet vero reprehenderit,
-      explicabo quaerat minima cum id! Optio distinctio architecto molestias
-      quos illo necessitatibus quisquam, nesciunt incidunt reiciendis ipsa,
-      harum quia illum eum. Labore deserunt dolor debitis, doloremque recusandae
-      neque eveniet ex in dignissimos, eum nesciunt consequatur, aspernatur
-      nihil corporis? Earum accusamus tempore officia nobis! Magnam ratione
-      soluta error, rem dolorem expedita excepturi cumque beatae culpa fugiat
-      officiis, nihil asperiores? Distinctio similique ea quia dolorum non
-      culpa!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis sit
-      voluptates quis alias molestias accusamus veniam, at aspernatur temporibus
-      dicta, est veritatis cupiditate dolorum ullam voluptas quaerat vitae
-      excepturi corrupti. Architecto aliquam eveniet vero reprehenderit,
-      explicabo quaerat minima cum id! Optio distinctio architecto molestias
-      quos illo necessitatibus quisquam, nesciunt incidunt reiciendis ipsa,
-      harum quia illum eum. Labore deserunt dolor debitis, doloremque recusandae
-      neque eveniet ex in dignissimos, eum nesciunt consequatur, aspernatur
-      nihil corporis? Earum accusamus tempore officia nobis! Magnam ratione
-      soluta error, rem dolorem expedita excepturi cumque beatae culpa fugiat
-      officiis, nihil asperiores? Distinctio similique ea quia dolorum non
-      culpa!
+      <p className={styles.title}>
+        is it better to work at home than in an office. do you agree?
+      </p>
+      <p>
+        In today's world of work, there's a big question: is it better to work
+        from home or in an office? This essay will look at three main things:
+        being with people, when you work, and how comfortable you are.
+      </p>
+      <p>
+        First,being in an office means you're around others. This helps with
+        teamwork and sharing ideas. But if you work from home, you might feel
+        lonely, which can make it harder to work together.
+      </p>
+      <p>
+        Second, working from home means you can choose when to work. This can
+        help balance work and life better. and also it's important to stay
+        focused on the office.
+      </p>
+      <p>
+        Lastly, feeling comfortable is important. When you are working from
+        home, you have the freedom to arrange your workspace to suit your
+        preferences. Eliminating travel time saves time and reduces stress.
+      </p>
+      <p>
+        In conclusion, both ways have good points and bad points. The best
+        choice depends on your job and what you like. Maybe doing both could be
+        good, so you can enjoy the good things about each and work at home and
+        in an office at the same time.
+      </p>
     </div>
   );
 }
